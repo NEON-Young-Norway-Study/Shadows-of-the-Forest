2b6437ae2855b3e845728bf844f23f9531a8062b
Andreas K.W. <Andreas.K.Wahlvaag@gmail.com>
2025-10-22 12:19:21 +0200

    WHY WON'T IT LESS HUNGER WHEN FOOOOOOOOD!

--- Assets/_Team/AKW/HungerManager.cs
+++ Assets/_Team/AKW/HungerManager.cs
@@ -24,6 +24,7 @@ public class HungerManager : MonoBehaviour
 
         if (Input.GetKeyUp(KeyCode.L))
         {
+            Debug.Log("Food!");
             hunger += foodRefillPerSec * Time.deltaTime;
         }
     }
