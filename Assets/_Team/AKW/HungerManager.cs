f9c59267dff53e3994dd47edeee2cc5938da00b9
Andreas K.W. <Andreas.K.Wahlvaag@gmail.com>
2025-10-23 09:58:47 +0200

    Script comments
    for easier understanding

--- Assets/_Team/AKW/HungerManager.cs
+++ Assets/_Team/AKW/HungerManager.cs
@@ -5,13 +5,13 @@ using UnityEngine.InputSystem;
 public class HungerManager : MonoBehaviour
 {
 
-    public Slider hungerbar;
+    public Slider hungerbar; // the display for the food meter
 
-    public float hunger;
-    public float maxHunger = 100f;
-    public float foodRefill = 10f;
-    public float hungerPerAct = 10f;
-    public float foodReserves = 3f;
+    public float hunger; // current % food
+    public float maxHunger = 100f; // 100%
+    public float foodRefill = 10f; // how much % to add when eating, need change or transfer when food elements are added
+    public float hungerPerAct = 10f; // food % going down when action
+    public float foodReserves = 30f; // how many time you can refill food, need change later
 
     void Start()
     {
@@ -22,18 +22,18 @@ public class HungerManager : MonoBehaviour
     {
         hungerbar.value = hunger;
 ;
-        if (Input.GetKeyUp(KeyCode.Q))
+        if (Input.GetKeyUp(KeyCode.Q)) // hunger goes down on action, might need seperate method
         {
             Debug.Log("ActionHaveHungry");
             hunger -= hungerPerAct;
         }
 
-        if (Input.GetKeyUp(KeyCode.L) && foodReserves > 0f)
+        if (Input.GetKeyUp(KeyCode.L) && foodReserves > 0f) // eat some food, need change and/or seperate method
         {
             Debug.Log("Food!");
             foodReserves -= 1f;
             hunger += foodRefill;
-            hunger = Mathf.Clamp(hunger, 0f, maxHunger);
+            hunger = Mathf.Clamp(hunger, 0f, maxHunger); // keeps food within 0-100 range
         }
 
         if (hunger <= 0)
--- Assets/_Team/AKW/MultibleEndingsManager.cs
+++ Assets/_Team/AKW/MultibleEndingsManager.cs
@@ -7,19 +7,21 @@ public class MultibleEndingsManager : MonoBehaviour
 
     public Image EndingDisplay;
 
+    [Header("endings")] //change to "scene-change" when their ready
     public Sprite badEnd;
     public Sprite nuteralEnd;
     public Sprite goodEnd;
 
     void Update()
     {
+        //add a if statment for "final day" when thats is ready
         if (hungerManager.hunger <= 20)
         {
             Debug.Log("bad ending");
             EndingDisplay.sprite = badEnd;
         }
 
-        if (hungerManager.hunger > 20 &&  hungerManager.hunger <= 80)
+        if (hungerManager.hunger > 20 && hungerManager.hunger <= 80)
         {
             Debug.Log("nuteral ending");
             EndingDisplay.sprite = nuteralEnd;
