0d381fbb3ba9e94b6ee4fb2fb0d84ac54827d16f
Andreas K.W. <Andreas.K.Wahlvaag@gmail.com>
2025-11-07 13:38:17 +0100

    Dialogue triggers and interaction base

--- Assets/_Scripts/Mikael/TriggerController.cs
+++ Assets/_Scripts/Mikael/TriggerController.cs
@@ -1,26 +1,44 @@
 using UnityEngine;
 
+//made by Mikael
+//added fetures by Andreas
+
 public class TriggerController : MonoBehaviour
 {
     [SerializeField] private GameObject ObjectToActivate;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Left mouse button is interaction")] 
+    public bool isNeedOfInteraction = false;
 
-    // Update is called once per frame
-    void Update()
-    {
+    private bool playerInside = false;
 
-    }
-    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
+            playerInside = true;
+
+            if (!isNeedOfInteraction)
+            {
+                ObjectToActivate.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isNeedOfInteraction && playerInside && Input.GetKeyDown(KeyCode.Mouse0))
         {
             ObjectToActivate.SetActive(true);
         }
     }
 }
+
