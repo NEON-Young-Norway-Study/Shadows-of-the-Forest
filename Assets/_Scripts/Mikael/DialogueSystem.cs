4f09eeaf8f832c69ed951caf50b6a6bea84bde03
Andreas K.W. <Andreas.K.Wahlvaag@gmail.com>
2025-11-07 10:16:09 +0100

    Dialogue prefab adjusted and tested
    evil SkullBush added

--- Assets/_Scripts/Mikael/DialogueSystem.cs
+++ Assets/_Scripts/Mikael/DialogueSystem.cs
@@ -11,14 +11,12 @@ public class DialogueSystem : MonoBehaviour
 
     private int lineIndex;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         dialogueText.text = string.Empty;
         StartDialogue();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
