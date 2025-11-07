cfddbc684a1fd3b48eb4ecf3e18ab2bb1a3f2f89
Einar <einarsterten@gmail.com>
2025-11-07 10:45:49 +0100

    Day 2 complete!

--- Assets/_Scripts/Einar/Berry_Minigame/Part2/BerryBasket.cs
+++ Assets/_Scripts/Einar/Berry_Minigame/Part2/BerryBasket.cs
@@ -1,3 +1,4 @@
+using System.Data.SqlTypes;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,10 @@ public class BerryBasket : MonoBehaviour
     [SerializeField] private string targetSceneName;
     [SerializeField] float counter;
     [SerializeField] float maxCollected;
+
+    [SerializeField] private string berryKey;
+    [SerializeField] private string requiredKey;
+    [SerializeField] private string finishedBerryKey;
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the object has the tag "Poisonous"
@@ -21,6 +26,11 @@ public class BerryBasket : MonoBehaviour
             Destroy(other.gameObject);
             if (counter == maxCollected)
             {
+                PlayerPrefs.DeleteKey(berryKey);
+                PlayerPrefs.SetString(requiredKey, "true");
+                PlayerPrefs.SetString(finishedBerryKey, "true");
+                PlayerPrefs.Save();
+
                 Cursor.visible = true;
                 SceneController.Instance.LoadScene(targetSceneName);
             }
--- Assets/_Scripts/Einar/SetSceneAndPrefs.cs
+++ Assets/_Scripts/Einar/SetSceneAndPrefs.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 
-public class SimpleSceneTrigger : MonoBehaviour
+public class SetSceneAndPrefs : MonoBehaviour
 {
     [SerializeField] private string targetSceneName;
-
+    [SerializeField] private string key;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             SceneController.Instance.LoadScene(targetSceneName);
         }
+    }
+
+    public void loadSceneAndSetPlayerPref()
+    {
+        PlayerPrefs.SetString(key, "true");
+        SceneController.Instance.LoadScene(targetSceneName);
     }
 }
