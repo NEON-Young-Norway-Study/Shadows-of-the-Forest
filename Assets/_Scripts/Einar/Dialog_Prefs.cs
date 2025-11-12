d572b3ecf9bf3cb6d0fa23d910786690225b8532
Einar <einarsterten@gmail.com>
2025-11-12 15:53:30 +0100

    update to dialog_pref, renaming and lists

--- Assets/_Scripts/Einar/Dialog_Prefs.cs
+++ Assets/_Scripts/Einar/Dialog_Prefs.cs
@@ -1,36 +1,42 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Dialog_Prefs : MonoBehaviour
 {
-    [SerializeField] private GameObject dialog_trigger_1;
-    [SerializeField] private GameObject dialog_trigger_2;
-    [SerializeField] private GameObject dialog_trigger_3;
+    [SerializeField] private List<GameObject> afterMinigameTriggers;
+    [SerializeField] private List<GameObject> afterSchoolTriggers;
+    [SerializeField] private List<GameObject> startOfDayTriggers;
 
     [SerializeField] string foodMinigame_Finished;
     [SerializeField] string school_Finished;
 
     void Start()
     {
+        if (PlayerPrefs.HasKey(school_Finished))
         {
-
-            if (PlayerPrefs.HasKey(foodMinigame_Finished))
-            {
-                dialog_trigger_1.SetActive(true);
-                dialog_trigger_2.SetActive(false);
-                dialog_trigger_3.SetActive(false);
-            }
-            else if (PlayerPrefs.HasKey(school_Finished))
-            {
-                dialog_trigger_1.SetActive(false);
-                dialog_trigger_2.SetActive(true);
-                dialog_trigger_3.SetActive(false);
-            }
-            else
-            {
-                dialog_trigger_1.SetActive(true);
-                dialog_trigger_2.SetActive(false);
-                dialog_trigger_3.SetActive(false);
-            }
+            SetActiveList(afterMinigameTriggers, false);
+            SetActiveList(afterSchoolTriggers, true);
+            SetActiveList(startOfDayTriggers, false);
+        }
+        else if (PlayerPrefs.HasKey(foodMinigame_Finished))
+        {
+            SetActiveList(afterMinigameTriggers, true);
+            SetActiveList(afterSchoolTriggers, false);
+            SetActiveList(startOfDayTriggers, false);
+        }
+        else
+        {
+            SetActiveList(afterMinigameTriggers, false);
+            SetActiveList(afterSchoolTriggers, false);
+            SetActiveList(startOfDayTriggers, true);
+        }
+    }
+    private void SetActiveList(List<GameObject> objects, bool isActive)
+    {
+        foreach (var obj in objects)
+        {
+            if (obj != null)
+                obj.SetActive(isActive);
         }
     }
 }
